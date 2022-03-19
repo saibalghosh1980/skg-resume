1e15cf1f70a4b0fd34bfd827356f1b3386ec663e
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-03-19 02:06:08 +0000

    Update WorkEx.tsx

--- src/components/pages/wokex/WorkEx.tsx
+++ src/components/pages/wokex/WorkEx.tsx
@@ -1,12 +1,77 @@
-import * as React from 'react';
+import axios from "axios";
+import * as React from "react";
+import {
+  Accordion,
+  Alert,
+  Card,
+  OverlayTrigger,
+  Spinner,
+  Tooltip,
+} from "react-bootstrap";
+import { useQuery } from "react-query";
 
-export interface IWorkExpProps {
-}
+export interface IWorkExpProps {}
 
-export default function WorkExp (props: IWorkExpProps) {
+export default function WorkExp(props: IWorkExpProps) {
+  
+  //=======================Fetch data=============================================================
+  const { isLoading, error, isError, data, isSuccess } = useQuery<any, Error>(
+    "SKG_WORKEX_SUMMARY",
+    () =>
+      axios(
+        `https://raw.githubusercontent.com/saibalghosh1980/saibalghosh1980.github.io/main/data/data-work-summary.json`
+      ),
+    {}
+  );
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error?.message}</Alert>;
+  }
+
+  if (isSuccess) {
+    console.log(data.data);
+  }
+  //==============================================================================================
   return (
-    <div>
-       Work Exp
-    </div>
+    <Accordion defaultActiveKey="0">
+      {data.data.workSummary.map(
+        (
+          item: {
+            name: React.ReactNode;
+            startDate: string;
+            endDate: string;
+            imageURL: string;
+          },
+          index: number
+        ) => (
+          <Card>
+            <Accordion.Toggle
+              as={Card.Header}
+              eventKey={index.toString()}
+              className="text-left"
+            >
+              <OverlayTrigger
+                placement="right"
+                delay={{ show: 250, hide: 400 }}
+                overlay={
+                  <Tooltip id="button-tooltip-2">
+                    [{item.startDate}&nbsp;-&nbsp;{item.endDate}]&nbsp;
+                  </Tooltip>
+                }
+              >
+                <img src={item.imageURL} height="25" />
+              </OverlayTrigger>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={index.toString()}>
+              <Card.Body>Hello! I'm the body - {index}</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        )
+      )}
+    </Accordion>
+    //<h1>{data.data.workSummary[0].name}</h1>
   );
 }
