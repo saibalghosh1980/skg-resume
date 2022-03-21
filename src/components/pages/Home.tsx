3aaaf8cd1bfd4aa7012e6555c1e9988063bfcdaf
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-03-21 15:23:04 +0000

    Update Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -9,17 +9,24 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import Alert from "react-bootstrap/Alert";
 import Spinner from "react-bootstrap/Spinner";
+import { Fade } from "react-bootstrap";
+import{CaretRightFill,CaretDownFill} from "react-bootstrap-icons";
 
 export interface IHomeProps {}
 
 export default function Home(props: IHomeProps) {
+  const [showExpertise, setShowExpertise] = React.useState<string>("none");
+  const handleShowHideClick = () => {
+    showExpertise === "block"
+      ? setShowExpertise("none")
+      : setShowExpertise("block");
+  };
   //=======================Fetch data=============================================================
   const { isLoading, error, isError, data, isSuccess } = useQuery<any, Error>(
     "SKG_EXEC_SUMMARY",
-    () => axios(`https://saibalghosh1980.github.io/data/data-exec-summary.json`),
-    {
-
-    }
+    () =>
+      axios(`https://saibalghosh1980.github.io/data/data-exec-summary.json`),
+    {}
   );
   if (isLoading) {
     return <Spinner animation="border" />;
@@ -39,7 +46,7 @@ export default function Home(props: IHomeProps) {
         <Col xs={3} className="text-left">
           <Image src={skgprofilepic} height="240" width="180" rounded />
         </Col>
-        <Col className="text-center">
+        <Col className="text-left">
           <div
             style={{
               whiteSpace: "pre-wrap",
@@ -48,6 +55,29 @@ export default function Home(props: IHomeProps) {
             }}
           >
             {data.data.execsummary}
+            <br />
+            <span
+              onClick={() => handleShowHideClick()}
+              style={{ cursor: "pointer" }}
+            >
+              {showExpertise === "none" ? <CaretRightFill color="royalblue"/> : <CaretDownFill color="royalblue"/>}
+              <b>Professional Expertise:</b>
+            </span>
+
+            <div
+              style={{
+                display: showExpertise,
+                fontSize: ".90rem",
+              }}
+            >
+              <ul>
+                {data.data.profesionalExpertise.map(
+                  (item: any, index: number) => (
+                    <li>{item.description}</li>
+                  )
+                )}
+              </ul>
+            </div>
           </div>
         </Col>
       </Row>
