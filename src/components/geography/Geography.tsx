1b2a626be75228ac53db6841e7a82d5d9524814b
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2022-04-06 00:19:28 +0100

    Update Geography.tsx

--- src/components/geography/Geography.tsx
+++ src/components/geography/Geography.tsx
@@ -5,6 +5,9 @@ import mapDataAsia from './mapDataAsia.js';
 import mapDataEU from './mapDataEU.js';
 import mapDataUS from './mapDataUS.js';
 import mapDataOC from './mapDataOC.js';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { Alert, Spinner } from 'react-bootstrap';
 
 require('highcharts/modules/map')(Highcharts);
 
@@ -15,27 +18,58 @@ export interface IGeogRaphyProps {
 }
 
 export default function GeogRaphy (props: IGeogRaphyProps) {
-    var asia = [
-        ['in', 4],
-        ['sg', 1],
-        ['bd', 1],
-        ['my', 1],
-        ['lk', 1],
-        ['cn', 3],
-      ];
-      var eu = [
-        ['dk', 1],
-        ['ie', .5],
-        ['gb', 5]
-      ];
-      var us = [
-        ['us-ca', 3],
-        ['us-ks', 1],
-        ['us-fl', 2]
-      ];
-      var oc = [
-        ['au', 2]
-      ];
+  
+  
+  var asia: any[]=[];
+  var eu: any[]=[];
+  var us: any[]=[];
+  var oc: any[]=[];
+
+  const addToContinent = (continent: string, data: any) => {
+    switch (continent) {
+      case 'asia':
+        asia.push(data);
+        break;
+      case 'eu':
+        eu.push(data);
+        break;
+      case 'us':
+        us.push(data);
+        break;
+      case 'oc':
+        oc.push(data);
+        break;
+      default:
+        break;
+    }
+  };
+
+  //=======================Fetch data=============================================================
+  const { isLoading, error, isError, data, isSuccess } = useQuery<any, Error>(
+    "SKG_GEO_SUMMARY",
+    () =>
+      axios(`https://saibalghosh1980.github.io/data/data-geographic-span.json`),
+    {}
+  );
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
+  if (isError) {
+    return <Alert variant="danger">{error?.message}</Alert>;
+  }
+
+  if (isSuccess) {
+    //console.log(data.data);
+    data.data.geoData.map((item:any)=>(
+      //console.log(item);
+      item.countries.map((item2:any)=>(
+         addToContinent(item.continent, [item2.countryName, parseFloat(item2.yoe)])
+      ))
+    ))
+  }
+  //==============================================================================================
+    
     
       const mapOptions = {
         title: {
